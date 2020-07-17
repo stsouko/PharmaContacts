0d529223d3c52346455c2eba9a324a4f257b2500
stsouko <nougmanoff@protonmail.com>
2020-07-17 13:21:30 +0300

    Initial commit

---
+++ README.rst
@@ -0,0 +1,9 @@
+PharmaContacts
+==============
+
+Pharmacophore contacts finder.
+
+COPYRIGHT
+=========
+
+2020 Ramil Nugmanov <nougmanoff@protonmail.com>
---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2020 Ramil Nugmanov <nougmanoff@protonmail.com>
+#  This file is part of PharmaContacts.
+#
+#  PharmaContacts is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, see <https://www.gnu.org/licenses/>.
+#
+from pathlib import Path
+from setuptools import setup
+
+
+setup(
+    name='PharmaContacts',
+    version='1.0.0',
+    packages=['PharmaContacts'],
+    url='https://github.com/stsouko/PharmaContacts',
+    license='LGPLv3',
+    author='Dr. Ramil Nugmanov',
+    author_email='nougmanoff@protonmail.com',
+    python_requires='>=3.6.0',
+    install_requires=['CGRtools>=4.0.32'],
+    zip_safe=True,
+    long_description=(Path(__file__).parent / 'README.rst').read_text(),
+    classifiers=['Environment :: Plugins',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Scientific/Engineering :: Chemistry',
+                 'Topic :: Scientific/Engineering :: Information Analysis',
+                 'Topic :: Software Development',
+                 'Topic :: Software Development :: Libraries',
+                 'Topic :: Software Development :: Libraries :: Python Modules'],
+)
